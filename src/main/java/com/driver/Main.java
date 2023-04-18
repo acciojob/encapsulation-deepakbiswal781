26098fe130fa058345e2cb2942eb7e02067f9d09
deepakbiswal781 <biswaldeepak781@gmail.com>
2023-04-18 11:25:06 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly t=new RWOnly();
+        t.setName("Deepak");
+        System.out.println(t.getName());
+
+    }
 }
